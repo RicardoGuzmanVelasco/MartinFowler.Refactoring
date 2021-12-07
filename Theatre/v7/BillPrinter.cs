44e65cf66d3e7a9bdf88e5c250cc3e9687aa8a52
RGV <ricardo.guzman.velasco@outlook.com>
2021-12-07 19:23:43 +0100

    v7b | Creation to other class (file in original Javascript code)

--- Theatre/v7/BillPrinter.cs
+++ Theatre/v7/BillPrinter.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 
 namespace MartinFowler.Refactoring.Theatre.v7
 {
@@ -9,73 +7,12 @@ namespace MartinFowler.Refactoring.Theatre.v7
     {
         public string Statement(Invoice invoice, IDictionary<string, Play> plays)
         {
-            return RenderPlainText(CreateStatementData(invoice, plays));
+            return RenderPlainText(StatementDataCreation.Create(invoice, plays));
         }
-
-        static StatementData CreateStatementData(Invoice invoice, IDictionary<string, Play> plays)
-        {
-            var data = new StatementData
-            {
-                invoiceCustomer = invoice.customer,
-                performances = invoice.performances.Select(EnrichPerformance)
-            };
-            data.totalAmount = TotalAmount(data);
-            data.totalVolumeCredits = TotalVolumeCredits(data);
-            return data;
-                
-            Performance EnrichPerformance(Performance aPerformance)
-            {
-                var result = aPerformance.Clone();
-                result.play = PlayFor(result);
-                result.amount = AmountFor(result);
-                result.volumeCredits = VolumeCreditsFor(result);
-                return result;
-            }
-            
-            
-            Play PlayFor(Performance aPerformance) => plays[aPerformance.playId];
-
-            int AmountFor(Performance aPerformance)
-            {
-                int result;
-                switch(aPerformance.play.type)
-                {
-                    case PlayType.Tragedy:
-                        result = 40000;
-                        if(aPerformance.audience > 30)
-                            result += 1000 * (aPerformance.audience - 30);
-                        break;
-                    case PlayType.Comedy:
-                        result = 30000;
-                        if(aPerformance.audience > 20)
-                            result += 10000 + 500 * (aPerformance.audience - 20);
-                        result += 300 * aPerformance.audience;
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-
-                return result;
-            }
-            
-            int VolumeCreditsFor(Performance aPerformance)
-            {
-                var result = Math.Max(aPerformance.audience - 30, 0);
-
-                if(PlayType.Comedy == aPerformance.play.type)
-                    result += (int)Math.Floor(aPerformance.audience / 5f);
-                
-                return result;
-            }
-        }
-
-        static int TotalAmount(StatementData data) => data.performances.Sum(perf => perf.amount);
-        static int TotalVolumeCredits(StatementData data) => data.performances.Sum(perf => perf.volumeCredits);
-
-        string RenderPlainText(StatementData data)
+        static string RenderPlainText(StatementData data)
         {
             var result = $"Statement for {data.invoiceCustomer}\n";
-            
+
             foreach(var perf in data.performances)
                 result += $" {perf.play.name}: {perf.amount / 100} ({perf.audience})\n";
 
@@ -83,8 +20,12 @@ namespace MartinFowler.Refactoring.Theatre.v7
             result += $"You earned {data.totalVolumeCredits} credits\n";
             return result;
         }
-        
-        string RenderHtml(StatementData data)
+
+        public string StatementHtml(Invoice invoice, IDictionary<string, Play> plays)
+        {
+            return RenderHtml(StatementDataCreation.Create(invoice, plays));
+        }
+        static string RenderHtml(StatementData data)
         {
             var result = $"<h1>Statement for {data.invoiceCustomer}</h1>\n";
 
@@ -95,6 +36,7 @@ namespace MartinFowler.Refactoring.Theatre.v7
                 result += $"<tr><td> {perf.play.name}</td><td>({perf.audience})</td>\n";
                 result += $"<td>{USD(perf.amount)}</td></tr>\n";
             }
+
             result += "</table>\n";
 
             result += $"<p>Amount owed is {USD(data.totalAmount)}</p>\n";
@@ -113,7 +55,7 @@ namespace MartinFowler.Refactoring.Theatre.v7
                 });
             }
         }
-        
+
         static string USD(float aNumber)
         {
             return (aNumber / 100).ToString(new CultureInfo("")
---
+++ Theatre/v7/StatementDataCreation.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MartinFowler.Refactoring.Theatre.v7
+{
+    internal static class StatementDataCreation
+    {
+        public static StatementData Create(Invoice invoice, IDictionary<string, Play> plays)
+        {
+            var data = new StatementData
+            {
+                invoiceCustomer = invoice.customer,
+                performances = invoice.performances.Select(EnrichPerformance)
+            };
+            data.totalAmount = TotalAmount(data);
+            data.totalVolumeCredits = TotalVolumeCredits(data);
+            return data;
+
+            Performance EnrichPerformance(Performance aPerformance)
+            {
+                var result = aPerformance.Clone();
+                result.play = PlayFor(result);
+                result.amount = AmountFor(result);
+                result.volumeCredits = VolumeCreditsFor(result);
+                return result;
+            }
+
+            Play PlayFor(Performance aPerformance) => plays[aPerformance.playId];
+
+            int AmountFor(Performance aPerformance)
+            {
+                int result;
+                switch(aPerformance.play.type)
+                {
+                    case PlayType.Tragedy:
+                        result = 40000;
+                        if(aPerformance.audience > 30)
+                            result += 1000 * (aPerformance.audience - 30);
+                        break;
+                    case PlayType.Comedy:
+                        result = 30000;
+                        if(aPerformance.audience > 20)
+                            result += 10000 + 500 * (aPerformance.audience - 20);
+                        result += 300 * aPerformance.audience;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                return result;
+            }
+
+            int VolumeCreditsFor(Performance aPerformance)
+            {
+                var result = Math.Max(aPerformance.audience - 30, 0);
+
+                if(PlayType.Comedy == aPerformance.play.type)
+                    result += (int)Math.Floor(aPerformance.audience / 5f);
+
+                return result;
+            }
+        }
+            static int TotalAmount(StatementData data) => data.performances.Sum(perf => perf.amount);
+            static int TotalVolumeCredits(StatementData data) => data.performances.Sum(perf => perf.volumeCredits);
+    }
+}
